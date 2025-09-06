38d1d9c84895d2b1c446bcd4497555ecc73ed048
Deepak Kumar Yadav <yadavdeepakk9212@gmail.com>
2025-09-06 07:51:38 +0530

    sortng algorithms done

---
+++ Array/bubbleSort.java
@@ -0,0 +1,29 @@
+public class bubbleSort {
+
+    public static void bubbleSorting(int arr []) {
+        for (int turn = 0; turn < arr.length - 1; turn++){
+            for (int j = 0; j < arr.length - 1 - turn; j++){
+                if(arr[j] > arr[j+1]) {
+                    // swap
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    swaps++;
+                }
+            }
+        }
+    }
+
+    
+    public static void printArr (int arr []) {
+        for(int i = 0; i< arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+    public static void main (String[] args) {
+        int arr[] = {1,3,2,5,6,4};
+        bubbleSorting(arr);
+        printArr(arr);
+        
+    }
+}
